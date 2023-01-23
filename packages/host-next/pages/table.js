baf6ea98d642c5cf3d24a5226f4b4ba9d5e2e38f
DemonDis <50297797+DemonDis@users.noreply.github.com>
2023-01-23 21:18:49 +0700

    :truck: routing

--- packages/host-next/pages/table.js
+++ packages/host-next/pages/table.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Link from 'next/link'
 
+import variables from '../styles/variables.module.scss';
+
 const Table = () => {
     return (
         <div>
             <h1>TABLE</h1>
+            <h1 style={{ color: variables.primaryColor }}>SCSS modu Next js</h1>
+            <h4 className='btn-text'>CSS Next js class</h4>
+            <h1 className="text-3xl font-bold underline p-3 ">Hello TAILWIND!</h1>
             <Link href='/'>HOME</Link>
         </div>
     )
