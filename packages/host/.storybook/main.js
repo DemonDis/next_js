a50f666f459005451bef5706d9a7edd5d4bb2ab7
DemonDis <dimas.ras@mail.ru>
2023-01-25 09:55:37 +0000

    :alembic: storybook-module-federation

--- packages/host/.storybook/main.js
+++ packages/host/.storybook/main.js
@@ -20,14 +20,14 @@ module.exports = withStorybookModuleFederation({
   },
 })({
   stories: [
-    '../stories/**/*.stories.mdx', 
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
+    '../src/**/*.stories.mdx', 
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-next'
+    '@storybook/preset-scss'
   ],
   framework: '@storybook/react',
   core: {
