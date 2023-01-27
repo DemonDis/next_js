70f278483e24b27aea34b139d0795bbf0405690c
DemonDis <dimas.ras@mail.ru>
2023-01-27 03:26:27 +0000

    :alembic: imports fonts

--- packages/host-next/pages/_app.js
+++ packages/host-next/pages/_app.js
@@ -1,10 +1,16 @@
+import dynamic from 'next/dynamic';
+import localFont from '@next/font/local';
+
 import '../styles/index.css';
 import '../styles/index.scss';
 
 import 'bootstrap/dist/css/bootstrap.css'
- import '../styles/globals.css'
+import '../styles/globals.css'
 
-import dynamic from 'next/dynamic';
+const myFont = localFont({ src: '../styles/fonts/Proxima/ProximaNovaCond-Black.woff2' });
+// const myFont = localFont({ src: '../styles/fonts/Inkulinati/Inkulinati-Regular.otf' });
+// const myFont = localFont({ src: '../styles/fonts/Truth/Truth.ttf' });
+
 console.log(__webpack_share_scopes__);
 const App = dynamic(
   async () => {
@@ -16,7 +22,11 @@ const App = dynamic(
 );
 
 function Home(props) {
-  return <App {...props} />;
+  return (
+    <main className={myFont.className}>
+      <App {...props} className={myFont.className}/>
+    </main>
+  )
 }
 
 export default Home;
