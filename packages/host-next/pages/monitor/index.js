6d90a8d1982a3974d88b04aeb71e5bdfd2f94a08
DemonDis <dimas.ras@mail.ru>
2023-01-27 04:07:38 +0000

    :alembic: add jpg next/image

--- packages/host-next/pages/monitor/index.js
+++ packages/host-next/pages/monitor/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Image from 'next/image'
+
+import profilePic from '../../public/assets/texture.jpg'
 
 import Header from '../../components/Header';
 import variables from '../../styles/variables.module.scss';
@@ -11,7 +14,12 @@ const Monitor = () => {
             <h1 style={{ color: variables.primaryColor }}>SCSS modul Next js</h1>
             <h4 className='btn-text'>CSS Next js</h4>
             <h1 className="text-3xl font-bold underline p-3 ">TAILWIND</h1>
-
+            <Image
+                src={profilePic}
+                alt="me"
+                width="100%"
+                height="100%"
+            />
         </div>
     )
 }
